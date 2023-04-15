9da5793d4efd840ae425c4bb122f7332efc053fe
Thirumala Sai Harsha Inumarthi <117017118+saiharsha3377@users.noreply.github.com>
2023-04-15 13:33:05 +0500

    Add files via upload

---
+++ Reverse pyramid print using nums.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int i, j, k, n = 5;
+    for (i = n; i >= 1; i--) {
+        k = i;
+        for (j = 1; j <= n-i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        for (j = i-1; j >= 1; j--) {
+            printf("%d", j);
+        }
+        printf("\n");
+    }
+    return 0;
+}
+
+//output:
+
+123454321
+ 1234321
+  12321
+   121
+    1
