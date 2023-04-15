7158df9ca956fd1ca38eaa92b47417259129061f
Thirumala Sai Harsha Inumarthi <117017118+saiharsha3377@users.noreply.github.com>
2023-04-15 13:42:18 +0500

    Add files via upload

---
+++ Fun with patterns.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    printf("     '''''''''' \n");
+    printf("   '  _     _  '\n");
+    printf("  '  (o)   (o)  '\n");
+    printf(" '      ~~~      '\n");
+    printf("'   \\         /   '\n");
+    printf(" '   \\       /   '\n");
+    printf("  '   \\     /   '\n");
+    printf("   '   \\   /   '\n");
+    printf("     '''''''''' \n");
+
+    return 0;
+}
+
+//output:
+
+     ''''''''''
+   '  _     _  '
+  '  (o)   (o)  '
+ '      ~~~      '
+'   \         /   '
+ '   \       /   '
+  '   \     /   '
+   '   \   /   '
+     ''''''''''
