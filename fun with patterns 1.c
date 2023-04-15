604157c123c26cd245a6f91f61dba92c82081771
Thirumala Sai Harsha Inumarthi <117017118+saiharsha3377@users.noreply.github.com>
2023-04-15 13:52:25 +0500

    Add files via upload

---
+++ fun with patterns 1.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("    O\n");
+    printf("   /|\\ \n");
+    printf("   / \\ \n");
+    printf("  /   \\ \n");
+    printf(" /     \\ \n");
+    printf("/       \\ \n");
+
+    return 0;
+}
+
+//output:
+    O
+   /|\
+   / \
+  /   \
+ /     \
+/       \
